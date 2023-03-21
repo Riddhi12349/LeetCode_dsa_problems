9250858df7d218feb8819d02d94a107b0bfb938d
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-03-21 18:08:58 +0530

    Time: 171 ms (68.78%), Space: 107.6 MB (54.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long subarray = 0;
+        long long subarray_size = 0;
         long long ans = 0;
         for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i] == 0){
-                subarray += 1;
-                ans += subarray;
+                subarray_size += 1;
+                ans += subarray_size;
             }
             else
            {
-               subarray = 0;
+               subarray_size = 0;
            }
         }
         return ans;
