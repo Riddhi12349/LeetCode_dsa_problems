7f22eb18fb18526f2dffbcf2608aab1cbc8ed6b8
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-05-05 11:13:54 +0530

    Time: 13 ms (97.59%), Space: 10 MB (29.12%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int maxVowels(string s, int k) {
+     
+     
+      int maxi = 0 , cnt = 0 , i = 0 , j = 0;
+      while(j < s.size()){
+   if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u'){
+          cnt++;
+   }
+   if(j==(i+k-1)){
+       maxi = max(maxi , cnt);
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
+          cnt--;
+   } 
+   i += 1;
+        }
+    j++;
+}
+// maxi = max(maxi , cnt);
+// int p;
+//  (maxi >= k) ? (p = k) : p = maxi;
+//  return p;   
+    return maxi;
+    }
+};
