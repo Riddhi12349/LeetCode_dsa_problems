5474173ad80b1af569b709e2a177e2480c367fdc
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-05-27 16:01:04 +0530

    Time: 5 ms (96.02%), Space: 10.3 MB (87.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,6 +4,7 @@
 //= TC -> O(NLOGN) , SC -> O(1) USING BINARY SEARCH****
 class Solution {
 public:
+/****** (SOLN-1) *********
     int helper(vector<int>& v1 , int n , int i , int prv , vector<vector<int>>& dp){
 			if(i >= n){ 
 				return 0;
@@ -19,6 +20,7 @@ public:
 
 	 return dp[i][prv] = max(p,q);
 		}
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 				vector<int> v1(n+1 , -10001);
@@ -30,4 +32,50 @@ public:
 		 return helper(v1 , n+1 , 1 , 0 , dp);
 	
     }
+    */
+
+ /****** (SOLN-2) *******
+    int lengthOfLIS(vector<int>& nums){
+       
+        int n = nums.size();
+        vector<int> dp(n ,  0);
+        dp[0] = 1;
+     
+        for(int i = 1 ; i < n ; i++){
+            int maxim = 0;
+            for(int j = i-1 ; j >= 0 ; j--){
+                if(nums[i] > nums[j]){
+                    if(maxim < dp[j]){ maxim = dp[j];}
+                }
+            }
+            dp[i] = maxim + 1;
+        }
+
+        int ans_max = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(ans_max < dp[i]){
+                ans_max = dp[i];
+            }
+        }
+        return ans_max;
+    }
+    */
+
+    /****** (SOLN-3) *******/
+
+     int lengthOfLIS(vector<int>& nums){
+         vector<int> temp;
+         temp.push_back(nums[0]);
+
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+    auto indx = lower_bound(temp.begin() , temp.end() , nums[i]); // iterator
+    temp[indx - temp.begin()] = nums[i];
+            }
+        }
+        return temp.size();
+     }
 };
