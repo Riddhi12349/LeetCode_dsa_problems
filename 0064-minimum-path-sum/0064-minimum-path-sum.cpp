9d48d2e2fb627cdd86ecb97d6912d86d77b34186
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-05-29 16:48:42 +0530

    Time: 18 ms (6.02%), Space: 10.1 MB (49.12%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+/****** dp TABLE ********
+    int helper(vector<vector<int>>& grid , int i , int j , vector<vector<int>>& dp , int m , int n) {
+*/
+        /*
+    dp[0][0] = grid[0][0];
+  for(int j = 1 ; j < n ; j++){
+      dp[0][j] = dp[0][j-1] + grid[0][j];
+  }
+  for(int i = 1 ; i < m ; i++){
+      dp[i][0] = dp[i-1][0] + grid[i][0];
+  }
+     for(int i = 1 ; i < m ; i++){
+       for(int j = 1 ; j < n ; j++){
+        dp[i][j] = grid[i][j] + min(dp[i-1][j] , dp[i][j-1]);
+               }
+           }
+
+           return dp[m-1][n-1];
+    }
+*/
+        int helper(vector<vector<int>>& grid , int i , int j , vector<vector<int>>& dp , int m , int n) {
+      
+        if(i == m-1 && j == n-1){
+            return grid[i][j];
+        }
+        if( i >= m|| j >= n || i < 0 || j < 0){
+           // return 201;
+            return INT_MAX;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int x = grid[i][j];
+   
+
+int p = x + min(helper(grid , i , j+1 , dp , m , n) , helper(grid , i+1 , j , dp , m , n));
+        
+        dp[i][j] =  p;
+        return p;
+}
      
-        int m = grid.size() , n = grid[0].size();
+    int minPathSum(vector<vector<int>>& grid) {
+    int m = grid.size() , n = grid[0].size();
+     vector<vector<int>> dp(m , vector<int>(n , -1));   
 
-        vector<vector<int>> ans(m , vector<int>(n , 0));
-
-        ans[0][0] = grid[0][0];
-        for(int j = 1 ; j < n ; j++){
-            ans[0][j] = grid[0][j] + ans[0][j-1];
-        }
-
-        for(int i = 1 ; i < m ; i++){
-            ans[i][0] = grid[i][0] + ans[i-1][0];
-        }
-
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 1 ; j < n ; j++){
-                ans[i][j] = grid[i][j] + min(ans[i][j-1] , ans[i-1][j]);
-            }
-        }
-
-        return ans[m-1][n-1];
+    // vector<vector<int>> dp(m , vector<int>(n , 0));   
+       return helper(grid , 0 , 0 , dp , m , n);
     }
 };
