137edbacabcb46618eb0064a09882221727cdd31
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-06-15 16:20:36 +0530

    Time: 16 ms (10.78%), Space: 11.3 MB (64.24%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,85 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
-/*
-    Node* helperf(Node* head){
-        if(head == NULL){
-            return NULL;
-        }
-
-    Node* newHead = new Node(head->val);
-    newHead->next = helperf(head->next);
-    return newHead;
-    }
     Node* copyRandomList(Node* head) {
-        
-            Node* newHead = helperf(head);
-            Node* p = head;
-            Node* q = newHead;
-            int cnt = 0;
-            while(p != NULL){
-                Node* t1 = p->random;
-                Node* k = head;
-                cnt = 0;
-                while(k != t1){
-                    cnt++;
-                    k = k->next;
-                }
-                Node* t2 = newHead;
-                if(t1 != NULL){
-                    while(cnt != 0){
-                        t2 = t2->next;
-                        cnt--;
-                    }
-                q->random = t2;
-                }
-
-                else{
-                    while(t2 != NULL){
-                        t2 = t2->next;
-                    }
-                    q->random = t2;
-                }
-                q = q->next;
-                p = p->next;
-            }
-            return newHead;
-        
-    }
-    */
-
-    Node* copyRandomList(Node* head){
+       
         unordered_map<Node* , Node*> map;
         Node* p = head;
+        Node *h = NULL;
         while(p != NULL){
             map[p] = new Node(p->val);
+            if(h == NULL){
+                h = map[p];
+            }
             p = p->next;
         }
-
         p = head;
         while(p != NULL){
-
-        Node* newNode = map[p];
-        newNode->next = map[p->next];
-        newNode->random = map[p->random];
-
-        p = p->next;
+            map[p]->random = map[p->random];
+            map[p]->next = map[p->next];
+            p = p->next;
         }
-        return map[head];
+    return h;
     }
 };
