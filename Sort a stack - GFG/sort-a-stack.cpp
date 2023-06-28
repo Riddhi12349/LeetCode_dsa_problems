b42d0d7f357a621f77e0103b6c99b86e7bcd7c11
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-06-28 10:37:55 +0530

    Added solution - LeetHub

---
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort()
+{
+   //Your code here
+   
+   if(s.empty()){
+       return ;
+   }
+   
+   int temp = s.top();
+   s.pop();
+   sort();
+  
+   stack<int> s2;
+
+    while(!s.empty() && s.top() > temp){
+        s2.push(s.top());
+        s.pop();
+    }
+   
+   s.push(temp);
+   while(!s2.empty()){
+       s.push(s2.top());
+       s2.pop();
+   }
+}
