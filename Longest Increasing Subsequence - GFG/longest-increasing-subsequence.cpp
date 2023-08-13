849e3f8c42a5557c0a376cbb4250bc857c8e366c
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-08-13 08:16:30 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 // } Driver Code Ends
 
+  //USING BINARY SEARCH AND USING RECURSION
 
 class Solution
 {
     public:
+    // IF YOU WNT TO USE RECURSION THEN USE 1 MORE VARIABLE I.E PRV-INDX
   /*  int dp[10000];
     //Function to find length of longest increasing subsequence.
     int find(int a[] , int n , int i , vector<int>& res){
@@ -35,71 +37,24 @@ class Solution
        return dp[i] = max(len1 , len2);
     }*/
 
-int lengthOfLIS(int nums[] , int n)
-{
- 
-    // Binary search approach
-   // int n = nums.size();
-    vector<int> ans;
- 
-    // Initialize the answer vector with the
-    // first element of nums
-    ans.push_back(nums[0]);
- 
-    for (int i = 1; i < n; i++) {
-        if (nums[i] > ans.back()) {
- 
-            // If the current number is greater
-            // than the last element of the answer
-            // vector, it means we have found a
-            // longer increasing subsequence.
-            // Hence, we append the current number
-            // to the answer vector.
-            ans.push_back(nums[i]);
-        }
-        else {
- 
-            // If the current number is not
-            // greater than the last element of
-            // the answer vector, we perform
-            // a binary search to find the smallest
-            // element in the answer vector that
-            // is greater than or equal to the
-            // current number.
- 
-            // The lower_bound function returns
-            // an iterator pointing to the first
-            // element that is not less than
-            // the current number.
-            int low = lower_bound(ans.begin(), ans.end(),
-                                  nums[i])
-                      - ans.begin();
- 
-            // We update the element at the
-            // found position with the current number.
-            // By doing this, we are maintaining
-            // a sorted order in the answer vector.
-            ans[low] = nums[i];
-        }
-    }
- 
-    // The length of the answer vector
-    // represents the length of the
-    // longest increasing subsequence.
-    return ans.size();
-}
- 
+// USING BINARY SEARCH
 
    int longestSubsequence(int n, int a[]){
-//     {
-//     //     vector<int> res;
-//     //     memset(dp , -1 , sizeof(dp));
-//     //  int p = find(a , n , 0 , res);
-     
-
-//     //   return p;
-//     return lis(a , n);
-return lengthOfLIS(a , n);
+            
+            vector<int> ans;
+            ans.push_back(a[0]);
+            
+    for(int i = 1 ; i < n ; i++){
+        if(a[i] > ans.back())
+         ans.push_back(a[i]);
+        else{
+            int pos = lower_bound(ans.begin() , ans.end(), a[i])-ans.begin();
+            ans[pos] = a[i];
+        }
+    }
+    
+    return ans.size();
+    
   }
 };
 
