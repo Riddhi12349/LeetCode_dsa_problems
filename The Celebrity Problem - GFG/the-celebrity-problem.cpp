8ff218677710e3986bcd648f4a847e6bf7af839a
riddhitech <91315409+riddhitech@users.noreply.github.com>
2023-08-14 19:57:10 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -11,7 +11,8 @@ class Solution
 {
     public:
     //Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& M, int n) 
+   /* 
+   int celebrity(vector<vector<int> >& M, int n) 
     {
         vector<int> v1;
         for(int i = 0 ; i < M.size() ; i++){
@@ -35,6 +36,40 @@ class Solution
         }
         return indx;
     }
+    */
+      int celebrity(vector<vector<int> >& M, int n) {
+          
+          stack<int> st;
+          for(int i = 0 ; i < n ; i++){
+              st.push(i);
+          }
+          
+          // BASED ON ELIMINATION METHOD USING STACK
+          
+          while(st.size() != 1){
+              int i = st.top(); st.pop();
+              int j = st.top(); st.pop();
+             
+              if(M[i][j] == 1){
+                  // if i knows j that means i is not celebrity
+                  st.push(j);
+              }
+              else if(M[i][j] == 0){
+                  // if i does not knows j that means j is not celebrity
+                  st.push(i);
+              }
+          }
+          
+          int possible = st.top();
+          
+          for(int i = 0 ; i < n ; i++){
+              if(i != possible && (M[possible][i]==1 || M[i][possible] == 0))
+               return -1;
+          }
+          
+          return possible;
+      }
+    
 };
 
 //{ Driver Code Starts.
