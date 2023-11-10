70f8b1231c25fec356a86923f254aeeba34d6d83
riddhi <91315409+RiddhiTechu@users.noreply.github.com>
2023-11-10 13:07:13 +0530

    Time: 176 ms (52.56%), Space: 164.5 MB (66.24%) - LeetHub

---
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxNonDecreasingLength(vector<int>& a1 , vector<int>& a2) {
+        
+        //RELATE THIS QS TO LONGEST - INCREASING - SUBSEQUENCE
+        int  n = a1.size();
+        vector<int> len1(n , 1) , len2(n , 1);
+
+        for(int i = 1 ; i < n ; i++){
+            int len = 1;
+            if(a1[i] >= a1[i-1])
+             len = max(len , 1 + len1[i-1]);
+            if(a1[i] >= a2[i-1])
+             len = max(len , 1 + len2[i-1]);
+
+             len1[i] = len; 
+             len = 1;
+
+            if(a2[i] >= a1[i-1])
+              len = max(len , 1 + len1[i-1]);
+            if(a2[i] >= a2[i-1])
+              len = max(len , 1 + len2[i-1]);
+
+              len2[i] = len;
+            }
+
+        int mxlen1 = *max_element(len1.begin() , len1.end());
+        int mxlen2 = *max_element(len2.begin() , len2.end());
+
+        return max(mxlen1 , mxlen2);
+    }
+};
