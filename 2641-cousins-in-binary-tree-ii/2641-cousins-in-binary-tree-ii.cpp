cb175cf18c06e262964c44bfb82371f53054ec02
riddhi <91315409+RiddhiTechu@users.noreply.github.com>
2024-01-02 13:07:15 +0530

    Time: 998 ms (5.11%), Space: 663 MB (5.11%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -1,78 +1,60 @@
 class Solution {
 public:
-    unordered_map <TreeNode* , TreeNode*> parent;
-    unordered_map <TreeNode* , TreeNode*> mp;
-    
+    unordered_map <TreeNode*,TreeNode*> parent , mp;
+
     void findParent(TreeNode* root , TreeNode* ptr){
-         
-        if(root == NULL || ptr == NULL) return;
-        
-        if(root->left){
+        if(root == NULL) return ;
+        mp[root]=ptr;
+
+        if(root->left) {
             parent[root->left] = root;
             ptr->left = new TreeNode(root->left->val);
-            mp[root->left] = ptr->left;
-        }
+            }
         if(root->right){ 
             parent[root->right] = root;
-            ptr->right = new TreeNode(root->right->val); 
-            mp[root->right] = ptr->right;
-        }
-
-            findParent(root->left , ptr->left);
-            findParent(root->right , ptr->right);
-
+            ptr->right = new TreeNode(root->right->val);
+            }
+            
+        findParent(root->left , ptr->left);
+        findParent(root->right , ptr->right);
     }
-    
-    // void print(){
-    //    for(auto x : mp){
-    //       cout << x.first->val << " " << x.second->val << endl;
-    //     }
-    // }
-    
+
     TreeNode* replaceValueInTree(TreeNode* root) {
-        
-        if(root == NULL) return NULL;
-        
-        TreeNode* newRoot = new TreeNode(root->val);
+        TreeNode* newRoot = new TreeNode(0);
         TreeNode* ptr = newRoot;
-        mp[root] = newRoot;
-        findParent(root , ptr);
-        
-        // print();
        
+        findParent(root , ptr);
+     
+        //bfs
         queue<TreeNode*> q;
         q.push(root); 
-        
+  
         while(!q.empty()){
             
-            int s = q.size(); 
-            int sum = 0; 
-            vector<TreeNode*> nodes;
-            
+            int s = q.size();
+            queue<TreeNode*> q2; int sum=0;
+
             for(int i = 1 ; i <= s ; i++){
+               TreeNode* f = q.front();
+               q.pop();
+               sum += f->val;
+               q2.push(f);
                 
-                TreeNode* f = q.front(); 
-                q.pop();
-                if(f->left)  q.push(f->left);
-                if(f->right) q.push(f->right);
-
-                sum += f->val;
-                nodes.push_back(f);
+               if(f->left)  q.push(f->left);
+               if(f->right) q.push(f->right);
             }
-       
-            for(int i = 0 ; i < nodes.size() ; i++){
-                int tmpSum = sum ;
 
-                if(nodes[i] == root){ mp[nodes[i]]->val = 0; continue;}
+            while(!q2.empty()){
+                //cout <<"hello!" <<endl;
+               TreeNode* f = q2.front(); q2.pop();
+               if(f == root) continue;
 
-                tmpSum -= parent[nodes[i]]->left == NULL ? 0 : parent[nodes[i]]->left->val;
-             
-                tmpSum -= parent[nodes[i]]->right == NULL ? 0 : parent[nodes[i]]->right->val;
-                
-                mp[nodes[i]]->val = tmpSum;  
-            }          
+          int s1 = parent[f]->left ? parent[f]->left->val :0;
+          int s2 = parent[f]->right  ? parent[f]->right->val :0;
+          mp[f]->val = (sum - s1 - s2); 
+          // cout << f->val << " " << mp[f]->val << endl;
+                 }
         }
-        
         return newRoot;
     }
 };
