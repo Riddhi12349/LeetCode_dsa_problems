cfd6be933d116993e4780618fe57430be06a8cfe
riddhi <91315409+RiddhiTechu@users.noreply.github.com>
2024-01-06 17:50:45 +0530

    Time: 186 ms (59.39%), Space: 78.9 MB (50.11%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+  int find(int i , vector<int>& st , vector<int>& dp, vector<vector<int>>& jobs){
+        
+        int n = st.size();
+        
+        if(i >= n) return 0;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int nxt  = lower_bound(st.begin(),st.end(),jobs[i][1])-st.begin();
+        
+        int pick = jobs[i][2] + find(nxt , st, dp ,jobs);
+        int notPick = find(i+1 , st , dp , jobs);
+        
+        return dp[i] = max(pick , notPick);
+    }
+    
+    
+    int jobScheduling(vector<int>& st, vector<int>& et, vector<int>& pf) {
+     
+     int n = st.size();
+     
+      vector<vector<int>> jobs(n);
+      vector<int> dp(n,-1);
+        
+     for(int i = 0 ; i < n  ; i++)
+         jobs[i] = {st[i] , et[i] , pf[i]};
+     
+     sort(jobs.begin() , jobs.end());
+        
+     sort(st.begin() , st.end());
+        
+     int mx_profit = find(0 , st , dp , jobs);
+
+     return mx_profit;
+ }
+};
