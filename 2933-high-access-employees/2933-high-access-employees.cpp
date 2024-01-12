c1a015d9ca3a54db091fcb378461e967e0cfb334
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-01-12 06:28:00 +0530

    Time: 120 ms (18.13%), Space: 49.9 MB (19.95%) - LeetHub

--- 2933-high-access-employees/2933-high-access-employees.cpp
+++ 2933-high-access-employees/2933-high-access-employees.cpp
@@ -1,49 +1,39 @@
 class Solution {
 public:
-    void print(vector<string>& v1){
-        for(auto x : v1)
-           cout << x << " ";
-        cout << endl;
-    }
     vector<string> findHighAccessEmployees(
         vector<vector<string>>& at) {
-
-       vector<string> ans;
-       unordered_map <string,vector<string>> mp;
-
-       for(int i=0 ; i < at.size() ; i++){
-           mp[at[i][0]].push_back(at[i][1]);
-       } 
-
-       for(auto x : mp){
-           
-           vector<string> v2 = x.second;
-           sort(v2.begin(),v2.end());
-
-           for(int i=0 ; i < v2.size() ; i++){
-             
-             string s = v2[i];
-             string p = to_string(stoi(s.substr(0,2))+1);
+        
+        unordered_map <string,vector<string>> mp;
+        
+        for(int i=0 ; i < at.size() ; i++){
+            mp[at[i][0]].push_back(at[i][1]);
+        }
+        
+        vector<string> ans;
+        
+        for(auto x : mp){
             
-             string t ="";
-             if(p.size()==1) t += '0';
+            vector<string> v1 = x.second;
+            sort(v1.begin() , v1.end());
             
-              t += p + s.substr(2);
-             
-            int pos = lower_bound(v2.begin() , v2.end() , t)-v2.begin();
-
-            if(pos-1 >= 0 && v2[pos-1]< t){
-                if(pos-i >= 3)
-                  { cout << t << endl;
-                    ans.push_back(x.first);
-                   ///cout << pos<<" " <<i << endl;
-                  // print(v2);
-                   break; }
-            }
-
-           }
-       }
-
-       return ans;
+           for(int i=0 ; i < v1.size() ; i++){
+            
+               string tmp = v1[i].substr(0,2);
+               string a = to_string(stoi(tmp)+1);
+               string s="";
+               if(a.size()<2) {s="0";}
+               
+               string p  = s+a+v1[i].substr(2);
+                 cout<<p<<endl;
+               int pos = lower_bound(v1.begin(),v1.end(),p)-v1.begin();
+              cout<<x.first << " " << pos<<endl;
+               if((v1[pos-1] < p) && (pos-i>=3)){
+                   ans.push_back(x.first);
+                   break;
+               }
+               }
+        }
+        
+        return ans;
     }
 };
