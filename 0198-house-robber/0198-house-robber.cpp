f39abc7b472f9aef3bb97a960d637e3a3d60ec37
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-01-21 11:35:57 +0530

    Time: 4 ms (25.45%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-
-       int n = nums.size() , indx = 0; 
-      vector<int>  dp(n , 0);
-      dp[0] = nums[0];
-      for(int i = 1 ; i < nums.size() ; i++){
-          if(i == indx + 1){
-              dp[i] = max(dp[i-1] , nums[i]);
-          }
-          else{
-              dp[i] = max(dp[i-1] , nums[i] + dp[indx]);
-              indx++;
-          }
-      }
-      return dp[n-1];
+    int dp[105];
+    
+    int find(int i , vector<int>& a){
+        
+        if(i >= a.size()) return 0;
+        
+        if(dp[i]!=-1) return dp[i];
+        
+        int t = a[i] + find(i+2, a);
+        int p = find(i+1 , a);
+        
+        return dp[i]= max(t,p);
+    }
+    
+    int rob(vector<int>& a) {
+        
+        int n = a.size();
+        memset(dp , -1 , sizeof(dp));
+        
+        return find(0,a);
     }
 };
