bea2e16399e9d91dbee643c164160b160d9e9699
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-02-04 06:32:22 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (29.61%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
    
-    map <vector<int>,int> mp;
+    vector<vector<int>> ans;
     
-    void find(int k, int n, vector<int> v1 , int st){
-        
-        if(v1.size()==k){
-            
-            if(n==0){
-                /*
-                cout<<"print\n";
-                for(auto x : v1)
-                    cout << x << " ";
-                cout<< endl;
-                */
-                mp[v1]++; 
-            }
-            return;
-        }
+    void find(int st,int sum, int& k, int& tar, vector<int>& v1){
+     
+     if(sum>tar) return;
+     if(v1.size()==k){ 
+       if(sum==tar){
+         ans.push_back(v1);
+       }
+         return;}
+     if(st>9 || v1.size()>k) return;
         
      for(int i = st ; i <= 9 ; i++){
-        if(n-i<0) return;
-         v1.push_back(i); //cout <<i<<" ";
-         find(k,n-i,v1,i+1);
+         if(sum+i>tar) break;
+         v1.push_back(i); 
+         find(i+1,sum+i ,k,tar,v1);
          v1.pop_back();
      } 
-        cout<<endl;
+      
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
         
-        vector<vector<int>> ans;
         
         vector<int> v1;
-        find(k,n,v1,1);
+        find(1,0,k,n,v1);
         
-        for(auto x :mp)
-            ans.push_back(x.first);
-        
+      
         return ans;
     }
 };
