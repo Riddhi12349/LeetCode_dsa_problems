c90d3345ae1e6cc4bb5fe067ac06720510ea75cb
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-02-14 21:59:07 +0530

    Time: 140 ms (85.51%), Space: 126.4 MB (42.43%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,19 +5,17 @@ public:
         int n = a.size();
         vector<int> v1(n,0) ;
           
-        int j = 0;
+       int j_pos = 0 , j_neg = 1;
+        
         for(int i = 0 ; i < n ; i++){
             if(a[i] > 0){
-                v1[j] = a[i]; j += 2; }
-        }       
-        
-        j = 1;
-        for(int i = 0 ; i < n ; i++){
+                v1[j_pos] = a[i]; j_pos += 2; }
+      
             if(a[i] < 0){
-                v1[j] = a[i]; j += 2;
+                v1[j_neg] = a[i]; j_neg += 2;
             }
         }
-       
+        
         return v1;
     }
 };
