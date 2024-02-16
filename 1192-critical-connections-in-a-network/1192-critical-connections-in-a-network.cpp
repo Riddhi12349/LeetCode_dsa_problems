3a5fba0ed2d7149ce3b7838e32fc572108a72782
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-02-16 19:33:38 +0530

    Time: 450 ms (59.18%), Space: 182.2 MB (28.32%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> bridges;
     int timer = 1;
+    vector<vector<int>> bridges;
     
-    void find(int src , int parent, vector<int>& tin , vector<int>& tlow , vector<int>& vis , int n, 
-             vector<int> adj[]){
-        
-        vis[src] = 1; tin[src] = timer; tlow[src] = timer;
+    void dfs(int src , int parent , vector<int>& tin, vector<int>& tlow ,  vector<int>& vis, vector<vector<int>>& adj){
+         
+        vis[src] = 1;  tin[src] = timer ; tlow[src] = timer;
         timer++;
-        for(auto neighbor : adj[src]){
-            if(neighbor == parent) continue;
-            
-            if(!vis[neighbor]){
-              find(neighbor , src, tin , tlow , vis, n , adj);
-            
-             tlow[src] = min(tlow[src] , tlow[neighbor]);
-                
-            if(tlow[neighbor] > tin[src])
-                  bridges.push_back({src , neighbor});    
-        }
-            else {
-           // if already visited, then there is no qs of bridge--
-         // b/c by some other path that node has been visited
-            tlow[src] = min(tlow[src] , tlow[neighbor]);
-        }
-    }
-    }
-    vector<vector<int>> criticalConnections(int n, 
-                              vector<vector<int>>& conn) {
         
-     vector<int> tin(n , INT_MAX) , tlow(n ,INT_MAX) , vis(n , 0);
-     
-     vector<int> adj[n];
-     for(int i=0 ; i < conn.size() ; i++){
-         adj[conn[i][0]].push_back(conn[i][1]);
-         adj[conn[i][1]].push_back(conn[i][0]);
-     }
+        for(auto adj_el : adj[src]){ 
+            if(adj_el == parent) continue; //impt_step**********
+            if(!vis[adj_el]){  
+                dfs(adj_el , src , tin, tlow , vis, adj);              
+                tlow[src] = min(tlow[src] , tlow[adj_el]);
+                if(tlow[adj_el] > tin[src]){
+                    bridges.push_back({src, adj_el});
+                }
+            }
+            else{
+                tlow[src] = min(tlow[src] , tlow[adj_el]);
+            }
+        }
         
-     find(0 , -1 ,tin , tlow, vis , n , adj);
-     return bridges;
-       
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& conn) {
+        
+        vector<vector<int>> adj(n);
+        for(int i = 0 ; i < conn.size() ; i++){
+            adj[conn[i][0]].push_back(conn[i][1]);
+            adj[conn[i][1]].push_back(conn[i][0]);
+        }
+        
+        vector<int> tin(n ,INT_MAX) , tlow(n, INT_MAX) , vis(n , 0);
+        
+        dfs(0,-1,tin,tlow,vis,adj);
+        return bridges;
     }
 };
