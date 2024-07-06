6570ed192ae3cfaf6d8b3cd9467a366566d583ee
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-07-06 09:26:41 +0530

    Time: 605 ms (17.54%), Space: 214 MB (5.26%) - LeetHub

--- 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.cpp
+++ 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-  
-    int minimumSeconds(vector<int>& nums) {
+    int minimumSeconds(vector<int>& a) {
         
-        int n = nums.size();
+        int n = a.size();
+        for(int i = 0 ; i < n ; i++){
+            a.push_back(a[i]);
+        }
         
-        vector<int> a(nums.begin(),nums.end());
-        for(int i = 0 ; i < n ; i++)
-            a.push_back(nums[i]);
+        unordered_map <int,vector<int>> mp;
         
-               
-        unordered_map<int,vector<int> > mp;
         for(int i = 0 ; i < a.size() ; i++){
             mp[a[i]].push_back(i);
         }
         
-//        print(mp);
+        int time = INT_MAX;
         
-        int mnTime = INT_MAX;
-        
-        for(auto x : mp){
+        for(auto x  : mp){
             
-            int mxlen = 0;
-            vector<int> v = x.second;
-            for(int i = 0 ; i < v.size()-1 ; i++){
-                mxlen = max(mxlen , v[i+1]-v[i]-1);
+            vector<int> indxs = x.second;
+            int mxtime = 0; // maxm possible b/w all adjacents to convert to a single element
+            
+            for(int i = 0 ; i < indxs.size()-1 ; i++){
+                mxtime = max(mxtime , (indxs[i+1]-indxs[i])/2);
             }
-            mnTime = min(mnTime , (mxlen+1)/2);
+            
+            time = min(time , mxtime); // among all maximums who has minimum
         }
         
-           return mnTime;    
-        }
+        return time;
+    }
 };
