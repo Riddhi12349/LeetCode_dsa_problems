7486cdbfe0e128441a09716300c3f240d0dfcff0
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-07-10 09:09:09 +0530

    Time: 7 ms (12.10%), Space: 11.4 MB (12.54%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-      /*  if(n == 1){
-            return 0;
-        }
-        if(n == 2){
-            int x =  max(nums[0] ,nums[1]);
-            int t;
-            (x == nums[0]) ? t = 0 : t = 1;
-            return t;
-        }
-
-        if(nums[0] > nums[1]){ return 0; }
-
-        if(nums[n-1] > nums[n-2]){ return n-1; }
+    int findPeakElement(vector<int>& a) {
         
-        int h = n-2 , l = 1;
-        */
-        int l = 0 , h = n -1;
-        while(l < h){
-            int mid = (l + h)/2;
-
-            if(nums[mid] < nums[mid + 1]){
-                l = mid + 1;
-         
+        int n = a.size();
+        
+        if(n==1) return 0;
+        
+        //checking edge cases
+        if(a[0] > a[1]){ return 0; }
+        if(a[n-1] > a[n-2]) { return n-1; }
+        
+        int low = 1 , high = n-2;
+        
+        while(low <= high){
+            
+            int mid = (low + high)/2;
+            
+            if(a[mid] > a[mid-1] && a[mid] > a[mid+1]) 
+                return mid;
+            
+            else if( a[mid] < a[mid+1]){
+ // 1,2,p=3              //increasing side
+                low = mid+1;
             }
-           /* else if(nums[mid] < nums[mid-1]){
-                h = mid - 1;
-             
-            }
+            
             else{
-             //   return nums[mid];
-             return mid;
+// p=8,7,6       
+                high = mid-1;
             }
-            */
-            else{ h = mid; }
         }
-        return l;
+        
+        return -1;
     }
 };
