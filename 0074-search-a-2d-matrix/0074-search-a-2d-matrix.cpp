5979d2dc088bd17f5e508bde3b0a17525ef064cc
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-07-11 20:35:38 +0530

    Time: 8 ms (22.04%), Space: 12.2 MB (10.51%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    int bsearch(vector<vector<int>>& matrix, int target) {
-      
-       int m = matrix.size();
-       int low = 0 , high = m-1 , ans = -1;
-      
-       while(low <= high){
-           
-           int mid = (low + high)/2;
-
-           if(matrix[mid][0] == target){
-               return mid;
-           }
-           else if(matrix[mid][0] > target){
-               high = mid-1;
-           }
-           else{
-               ans = mid;
-               low = mid+1;
-           }
-       }
-
-       return ans;
-    
-    }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
- int m = matrix.size() , n = matrix[0].size();
-  // finding row of the target element
-  
-  int pos1 = bsearch(matrix , target);
-  
- if(pos1 != -1 && binary_search(matrix[pos1].begin() , matrix[pos1].end() , target)){
-     return true;
- }
- return false;
-    }
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        
+        int m = mat.size() , n = mat[0].size();
+        
+        int row = -1;
+        
+        int low= 0, high = m-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(mat[mid][0] > target){
+                high = mid-1;
+            }
+            else{
+                row = mid;
+                low = mid+1;
+            }
+        }
+        
+        if(row == -1) return false;
+        
+        int col = -1;
+        
+         low= 0, high = n-1;
+         while(low <= high){
+             
+             int mid = (low + high)/2;
+             if(mat[row][mid] > target){
+                 high = mid-1;
+             }
+             else{
+                 col = mid;
+                 low = mid+1;                 
+             }
+         }
+        
+        if(col == -1 || col >= n) return false;
+        
+        if(mat[row][col] == target) return true;
+        return false;
+     }
 };
