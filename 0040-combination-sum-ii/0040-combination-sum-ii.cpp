f224e9a38d18f1adca3f8b1e378374432acb8783
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-07-14 12:32:07 +0530

    Time: 10 ms (16.53%), Space: 13.1 MB (33.01%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-   
-    void find(int i, int sum, int t, vector<int>& cd,vector<int>& v1){
+    
+    void find(int i , vector<int>& a , int t , vector<int>& v1){
         
-        int n = cd.size();
+        if(t < 0) return;
         
-        if(sum==t) {ans.push_back(v1); return; }
-        if(sum > t) return; //controlled_recursion step
-        if(i>=n) return;
-        
-        v1.push_back(cd[i]);
-        find(i+1,sum+cd[i],t,cd,v1);
-        v1.pop_back();
-        //avoid all occ of that element
-        int j=i+1;
-        for(;j<n;j++){
-            if(cd[j]!=cd[i])break;
+        if(t == 0){ 
+            ans.push_back(v1); return;
         }
-        find(j,sum,t,cd,v1);        
+        
+        if(i >= a.size()){
+          return;  
+        }
+        
+        v1.push_back(a[i]); 
+        
+        int j=i+1;
+        for(j =i+1 ; j < a.size() ; ){
+            if(a[j] == a[i]) { j++; }
+            else { break; }
+        }
+        find(i+1 , a , t-a[i] , v1);
+        v1.pop_back();
+        
+        find(j , a , t, v1);
     }
     
-    vector<vector<int>> combinationSum2(vector<int>& cd, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& a, int t) {
         
-        sort(cd.begin(),cd.end());
+        sort(a.begin() , a.end());
         
         vector<int> v1;
-        find(0,0,target,cd,v1);
+        find(0 , a, t , v1);
+        
         return ans;
+        
     }
 };
