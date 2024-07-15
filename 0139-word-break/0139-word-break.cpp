7b1c92357d88eed8b1d062caa226f5985349ca40
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-07-15 15:19:45 +0530

    Time: 29 ms (14.25%), Space: 24 MB (9.07%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
- #include <unordered_map>
-   
-int wordBreakHelper(string s,unordered_map<string,int> map ,int indx , vector<int>& dp){
-
-    if(indx == s.size()){
-        return 1;
-    }
-    if(dp[indx] != -1){ return dp[indx]; }
-
-    for(int i = indx ; i < s.size() ; i++){
-        string s1 = s.substr(indx , i+1-indx);
-        if(map.count(s1) > 0){
-            bool ans = wordBreakHelper(s , map ,  i+1 , dp);
-            if(ans == 1){
-            dp[indx] = ans;
-            return ans;
-        }
-    }
-}
+    unordered_map <string,int> mp;
+    int dp[1005];
     
-    dp[indx] = 0;
-    return dp[indx];
-}
+    int find(int i , string s){
+        
+        if(i >= s.size()) return true;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int n = s.size();
+        
+        for(int j = i ; j < n ; j++){
+            
+            string tmp = s.substr(i , j-i+1);
+            
+            if(mp[tmp] > 0){
+                
+                if(find(j+1 , s)){ return dp[i] = 1; }
+            }
+        }
+        
+        return dp[i] = 0;
+    }
+    
     bool wordBreak(string s, vector<string>& wordDict) {
-     
-     unordered_map <string , int> map;
-     for(int i = 0 ; i < wordDict.size() ; i++){
-         map[wordDict[i]] += 1;
-     }
-     int n = s.size();
-     vector<int> dp(n , -1);
-     return  wordBreakHelper(s  , map , 0 , dp);   
-
+       
+        for(auto x : wordDict) mp[x]++;
+        
+        memset(dp , -1 , sizeof(dp));
+        
+        return find(0 , s);
     }
 };
