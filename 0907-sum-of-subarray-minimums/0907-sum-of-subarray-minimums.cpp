784298865eb869fd772bfd01755f778ab788659c
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-07-20 12:13:51 +0530

    Time: 230 ms (5.74%), Space: 47.6 MB (11.78%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,34 +1,72 @@
 class Solution {
 public:
     const int mod = 1e9+7;
+    using ll = long long;
+    
+    vector<int> findprvNSE(vector<int>& a){
+        
+        vector<int> prv; stack<int> st;
+        
+        for(int i = 0 ; i < a.size() ; i++){
+            
+            while(!st.empty() && a[st.top()] >= a[i]){
+                st.pop();
+            }
+            
+            if(st.empty()) prv.push_back(-1);
+            else prv.push_back(st.top());
+            
+            st.push(i);
+        }
+        
+        return prv;
+    }
+    
+    vector<int> findnxtNSE(vector<int>& a){
+       
+        int  n = a.size();
+        stack<int> st;  vector<int> nxt(n , n);
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            
+            while(!st.empty() && a[st.top()] > a[i]){ st.pop(); }
+            
+            if(!st.empty()) nxt[i] = (st.top());
+            
+            st.push(i);
+        }
+        
+        return nxt;
+    }
+    
+    void print(vector<int>& v1){
+        
+        for(auto x : v1){
+            cout << x<< " ";
+        }
+        cout << endl;
+    }
     
     int sumSubarrayMins(vector<int>& a) {
         
-        int sum = 0 ;
         int n = a.size();
-        stack<pair<int,int> > st; 
-        vector<int> ans(n,0);
         
-        for(int i = n-1 ;  i >= 0 ; i--){
-             
-            while(!st.empty() && st.top().first > a[i])
-                st.pop();
+        vector<int> prvNSE = findprvNSE(a);
+        vector<int> nxtNSE = findnxtNSE(a);
+        
+        print(prvNSE); 
+        print(nxtNSE);
+        
+        ll ans = 0;
+        
+        for(int i=0 ; i < n ; i++){
             
-            if(!st.empty()){
-            int indx = st.top().second;
-            int t = (ans[indx] + a[i]*(indx-i))%mod;
-            ans[i] = t;}
-            else{
-                ans[i] = (a[i]*(n-i))%mod;
-            }
+            ll p = i-prvNSE[i];
+            ll q = nxtNSE[i]-i;
             
-            st.push({a[i],i});
+            ans = (ans + (ll)(p*q*a[i])%mod)%mod;
         }
         
-        for(auto x : ans){
-            sum += x;
-            sum = sum % mod;
-        }
-        return sum%mod;
+        return ans;
     }
 };
