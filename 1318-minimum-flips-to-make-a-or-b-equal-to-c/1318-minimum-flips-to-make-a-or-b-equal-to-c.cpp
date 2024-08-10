b0a686fddd8746330ae938a2b2e25587cb4a2ed8
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-08-10 15:02:56 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (5.10%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    void findBits(vector<int>& v1, int num){
-        
-        int i=0;
-        while(num > 0){
-            int r = num % 2;
-            v1[i] = r;
-            i++;
-            num = num/2;
-        }
-        
-    }
     int minFlips(int a, int b, int c) {
         
-        vector<int> a_bits(32 , 0) , b_bits(32 , 0) , c_bits(32 , 0) , x_bits(32 , 0);
-
-        int x = a|b;
-       
-        findBits(a_bits  , a);
-        findBits(b_bits  , b);
-        findBits(c_bits  , c);
-        findBits(x_bits  , x);
+        int flips = 0;
         
-        int ans=0;
-        
-        for(int i=0; i<32 ;i++){
-            if(c_bits[i] == x_bits[i]) continue;
-            else{
-                if(x_bits[i] == 0){
-                    ans++;
-                }
-                else{
-                    if(a_bits[i]==1 && b_bits[i]==1) ans +=2;
-                    else ans++;
-                }
+        while(a > 0 || b > 0 || c > 0){
+            
+            int r1 =  a % 2 , r2 = b % 2 , r4 = c % 2;
+            
+            if(r1 == 0 && r2 == 0 && r4 == 1){
+                flips++;
             }
+            else if(r1 == 1 && r2 == 0 && r4 == 0){
+                flips++;
+            }
+            else if(r1 == 0 && r2 == 1 && r4 == 0){
+                flips++;
+            }
+            else if(r1 == 1 && r2 == 1 && r4 == 0){
+                flips += 2;
+            }            
+              
+            a = a/2; b  = b/2; c = c/2;
         }
         
-        return ans;
+        return flips;
     }
 };
