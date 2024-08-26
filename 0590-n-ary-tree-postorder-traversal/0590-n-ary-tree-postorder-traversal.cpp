8ba70e6ff8a547e9239ba8c47071e04624b0770b
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-08-26 11:46:50 +0530

    Time: 12 ms (66.87%), Space: 15.3 MB (21.73%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -17,9 +17,11 @@ public:
     }
 };
 */
-
+//stack = [1,5,10,9,13]
+//[2,6,14,11,7,3,12,8,4,]
 class Solution {
 public:
+  
     void traverse(Node* root , vector<int>& ans){
         
         if(root == NULL) return;
@@ -33,9 +35,42 @@ public:
         }
     }
     vector<int> postorder(Node* root) {
-        
+     /*   
         vector<int> ans;
         traverse(root , ans);
         return ans;
+      */
+      
+      if(root == NULL) return {};
+        
+      vector<int> ans;
+      
+  
+      stack<pair<Node*,int>> st;
+      st.push({root , 0}); //[NODE,VIS]
+      
+      while(!st.empty()){
+            
+            auto p = st.top();
+            st.pop();
+            
+            if(p.second == 0){
+               
+               st.push({p.first , 1});       
+               
+               Node* topNode = p.first;
+               int size = topNode->children.size();
+               
+               for(int idx = size-1 ; idx >= 0 ; idx--){
+                  st.push({topNode->children[idx] , 0});
+               }
+            }
+            else{
+                 ans.push_back(p.first->val); 
+            }
+      }
+      
+      return ans;
+        
     }
 };
