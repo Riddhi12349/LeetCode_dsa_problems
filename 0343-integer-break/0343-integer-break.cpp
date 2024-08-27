f89748255639bd440590d922b0ce2768c75643d5
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-08-27 14:46:12 +0530

    Time: 2 ms (53.14%), Space: 7.4 MB (66.30%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int dp[60];
+    int dp[65];
     
     int find(int n){
-
-        if(n == 0)  return 1;
-              
-         if(dp[n] != -1) return dp[n];
         
-         int  ans = 1;
+        if(n==0) return 1;
         
-        for(int i = 1 ; i < n ; i++){
-              int prod = i * max(n-i , find(n-i));
-              ans = max(ans , prod);
-          }
-            
-        return dp[n] = ans;
+        if(dp[n] != -1) return dp[n];
+        
+        int ans = 1;
+        for(int i = 1 ; i <= n-1 ; i++){
+            ans = max(ans , i*max(n-i , find(n-i)));
+        }
+        
+       return dp[n] = ans;
     }
-    
     int integerBreak(int n) {
-         
-        memset(dp , -1 , sizeof(dp));
-        return find(n);
-        
+       
+       memset(dp , -1 , sizeof(dp));
+       return find(n);
     }
 };
