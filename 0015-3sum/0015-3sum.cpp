baad17578d3fe692a0872dca0dcb8c74f9031e30
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-09-11 19:32:55 +0530

    Time: 88 ms (29.19%), Space: 27.5 MB (75.49%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
-
-    vector<vector<int>> threeSum(vector<int>& nums) {
-       
+    vector<vector<int>> threeSum(vector<int>& a) {
+        
         vector<vector<int>> ans;
-     if(nums.size() == 0){ return ans; }
-    map<vector<int> ,int> map1;
-    
-        sort(nums.begin() , nums.end());
-               int n = nums.size();
-        for(int k = 0 ; k < n ;){
-           
-            int i = k+1;
-            int j = n - 1;
-
-            while(i < j){
-                int s = nums[i] + nums[j] + nums[k];
-                if(s == 0 ){
-                    // vector<int> v1;
-                    // v1.push_back(nums[i]);
-                    // v1.push_back(nums[j]);
-                    // v1.push_back(nums[k]);
-                //  ans.push_back(v1);
-             map1[{nums[i] , nums[j] , nums[k]}] += 1;
-         
-                    i++;
-                //    while(i <  n && nums[i] == nums[i-1]){ i++; }
-                    j--;
-                 //   while( j >= 0 && nums[j] == nums[j+1]){ j--; }
-                    
+        
+        sort(a.begin() , a.end());
+        
+        int n = a.size();
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            if(i-1 >= 0 && a[i] == a[i-1]) continue;
+            
+            int tar = 0-a[i];
+            
+            int  j = i+1 , k = n-1;
+            
+            while(j < k){
+                
+                int sum = a[j] + a[k];
+                
+                if(sum == tar){
+                    ans.push_back({a[i] , a[j] , a[k]});
+                    j++;
+                    k--;
                 }
-                else if(s < 0 ){ i++;}
-                else if(s > 0 ){j--;}
+                else if(sum > tar){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+                
+                while(j < k && j-1 > i && a[j]==a[j-1]){
+                    j++;
+                }
+                while(k > j && k+1 < n && a[k]==a[k+1]){
+                    k--;
+                }
             }
-         k++;
-      //  while(k < n && nums[k] == nums[k-1]){ k++; }
         }
-    for(auto itr = map1.begin() ; itr != map1.end() ; itr++){
-        ans.push_back(itr->first);
-    }
-    
-    
+        
         return ans;
+
     }
 };
