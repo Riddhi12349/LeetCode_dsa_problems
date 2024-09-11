f39d4ce1a32924d68db70795c60c0bc05d78781c
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-09-11 16:33:03 +0530

    Time: 10 ms (19.64%), Space: 16.5 MB (95.36%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -13,13 +13,13 @@ public:
             mxProd = max({mxProd , prod_mx , prod_mn});
             mnProd = min({mnProd , prod_mn , prod_mx});
             
-            cout << "first" << prod_mx <<  " " << prod_mn << endl;
+     //       cout << "first" << prod_mx <<  " " << prod_mn << endl;
             
             int tmp = prod_mx;
              prod_mx = max({prod_mx, prod_mn  , 1});            
              prod_mn = min({prod_mn , tmp , 1});
             
-            cout << "second" << prod_mx << " " << prod_mn << endl;
+     //       cout << "second" << prod_mx << " " << prod_mn << endl;
             
         }
         
