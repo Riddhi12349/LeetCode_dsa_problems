b863b9dbd85f692509bcad22083581eac70a9efd
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-09-18 14:47:03 +0530

    Time: 3 ms (93.74%), Space: 17.4 MB (14.46%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
-    
- static bool cmp(string a , string b){
-        return a+b > b+a;
+    static bool cmp(string s1 , string s2){
+        
+       int n1 = s1.size() , n2 = s2.size(); 
+      
+    if(n1==n2){
+       int i=0;
+       while(i < n1 && i < n2){
+           if(s1[i]==s2[i]){ i++; }
+           else if(s1[i]>s2[i]){ return true; }
+           else return false;
+       }
+    }
+    else {
+         return (s1+s2)>(s2+s1);
+        }
+        return true;
     }
     
     string largestNumber(vector<int>& nums) {
-      
-        vector<string> str_nums;
-        for(int i =0 ; i < nums.size() ; i++){
-            str_nums.push_back(to_string(nums[i]));
+        
+        vector<string> v1;
+        
+        int f=0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            
+            //check if nums->contains only zeros
+            if(nums[i] != 0) f=1;
+            v1.push_back(to_string(nums[i]));
         }
         
-       // sorting lexicographically not valid for given qs
-        // so we will sort the nos by adding the consecutive nos
-        sort(str_nums.begin() , str_nums.end() , cmp);
-        
-        string ans = "";
-        for(int i = 0 ; i < str_nums.size() ; i++){
-            ans += str_nums[i];
-        }
-        if(ans[0]== '0'){
+        if(f==0){
+            //means nums contains only string "0"
             return "0";
         }
-        return ans;
         
+        sort(v1.begin() , v1.end() , cmp); // descending order acc to digit pos
+        
+        
+        string ans = "";
+        for(int i = 0 ; i < v1.size() ; i++){
+            ans += v1[i];
+        }
+        /*
+        int i=0;
+        while(i<ans.size()){
+            if(ans[i]=='0') i++;
+            else break;
+        }
+        
+        ans = ans.substr(i);
+        return ans.size()==0 ? "0" : ans;
+        */
+        return ans;
     }
 };
