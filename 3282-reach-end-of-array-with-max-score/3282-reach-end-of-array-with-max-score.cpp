ae049f739ca191ac7baf4b64a4bf143ab27c4425
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-10-17 16:09:36 +0530

    Time: 218 ms (22.56%), Space: 171.5 MB (66.68%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
-    using ll = long long;
-   // ll dp[100005];
-    /*
-    ll find(int i , vector<int>& a){
+    long long findMaximumScore(vector<int>& nums) {
         
-        int n = a.size();
+        int n = nums.size(); long long score = 0;
         
-        if(i >= n-1) return 0;
+        int prv_i = 0;
         
-        if(dp[i] != -1) return dp[i];
+        for(int i = 1 ; i < n ; i++){
             
-        ll mxscore = 0;
-        
-        for(int j = i+1 ; j < n ; j++){
-            
-            ll score = 1ll*(j-i)*a[i] + find(j , a);
-            mxscore = max(mxscore , score);
-            
+            if(i==n-1 || nums[i] > nums[prv_i]){
+                score += (i-prv_i)*1ll*nums[prv_i];
+                prv_i = i;
+            }
         }
         
-        return dp[i] = mxscore;
-    }
-    */
-    
-    long long findMaximumScore(vector<int>& a) {
-        
-     //   memset(dp , -1 , sizeof(dp));
-     //   return find(0 , nums);
-     
-         ll score = 0;
-         int st_i = 0 , n = a.size();
-        
-         for(int j = 1 ; j < n ; j++){
-             if(a[j] > a[st_i]){
-                 score += 1ll*(j-st_i)*a[st_i];
-                 st_i = j;
-             }
-         }
-       
-        if(st_i != n-1){
-            score += 1ll*(n-1-st_i)*a[st_i];
-        }
         return score;
+        
     }
 };
