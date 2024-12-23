5bf720c07848eace8a810b153d738e7b4af9e8b0
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-12-23 17:38:10 +0530

    Time: 204 ms (23.16%), Space: 217.3 MB (14.95%) - LeetHub

--- 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -1,56 +1,57 @@
 class Solution {
-  public:
-   
-    int find(queue<TreeNode*> q){
+public:
+    int minSwaps(vector<int>& v1){
         
-        vector<int> v1;
-        while(!q.empty()){
-            v1.push_back(q.front()->val);
-            q.pop();
-        }
+        //a = [7,6,8,5] => [{5,0},{6,1},{7,2},{8,3}]
+         
+        int n = v1.size(), swaps=0;
         
-        vector<int> v2(v1.begin(),v1.end());
+        vector<int> v2(v1.begin() , v1.end());
+        
         sort(v2.begin() , v2.end());
-     
+        
         unordered_map<int,int> mp;
-        for(int i = 0 ; i < v2.size() ; i++){
-            mp[v2[i]] = i;
+        for(int i=0; i<n;  i++){
+            mp[v2[i]]=i;
         }
         
-        int swaps = 0;
-        for(int i = 0 ; i < v1.size() ; i++){
+        for(int i=0; i<n; i++){
             
             while(i != mp[v1[i]]){
-                int pos  = mp[v1[i]];
-                swap(v1[i] , v1[pos]);
+                swap(v1[i] , v1[mp[v1[i]]]);
                 swaps++;
             }
         }
-     
+        
         return swaps;
     }
     
     int minimumOperations(TreeNode* root) {
         
-        queue<TreeNode*> q;
-        q.push(root); int tot_ops = 0;
+        int totSwaps = 0;
         
-       while(!q.empty()){
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
             
-            int s = q.size(); queue<TreeNode*> q2;
-          
-         for(int i = 1 ; i <= s ; i++){
-            TreeNode* f = q.front();  q2.push(f);
-            q.pop();
-             
-            if(f->left) q.push(f->left);
-            if(f->right) q.push(f->right);
-         }
+              vector<int> v1;
+              int s = q.size();
             
-          tot_ops +=  find(q);
-     
+              for(int i = 1 ; i <= s ; i++){
+                  
+                  TreeNode* fr = q.front();
+                  q.pop();
+                  
+                  if(fr->left) q.push(fr->left);
+                  if(fr->right) q.push(fr->right);
+                  
+                  v1.push_back(fr->val);
+              }
+            
+              totSwaps += minSwaps(v1);            
         }
         
-        return tot_ops;
+        return totSwaps;
     }
 };
