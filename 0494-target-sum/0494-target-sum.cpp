fee4e0f02aee462dbfb871c0bdb5e3a232b6e28c
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-12-26 21:00:09 +0530

    Time: 7 ms (71.87%), Space: 11.8 MB (64.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    int dp[25][2500];
+    
     int find(int i , int sum , vector<int>& nums, int target){
         
         int n = nums.size();
@@ -9,6 +11,13 @@ public:
             return 0;
         }
         
+        if(sum >= 0 && dp[i][sum] != -1)
+            return dp[i][sum];
+        else if(sum < 0 && dp[i][sum+1001] != -1) 
+            return dp[i][sum+1001];
+            
+      //  if(dp[i][sum] != -1) return dp[i][sum];
+        
         int op1 = 0, op2 = 0;
         
         
@@ -16,10 +25,15 @@ public:
         
         op2 = find(i+1 , sum - nums[i] , nums, target); //add -
             
-        return op1 + op2;
+        if(sum >= 0) return dp[i][sum] = op1+op2;
+        else return dp[i][sum+1001] = op1+op2;
+        
+        //return op1 + op2;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
+        
+        memset(dp , -1 , sizeof(dp));
         
         return find(0,0,nums,target);
     }
